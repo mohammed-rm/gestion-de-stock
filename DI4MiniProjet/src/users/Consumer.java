b34ba696a40c54ab7da5ac6027ba2866d475e7b5
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-06 14:49:19 +0200

    .

--- DI4MiniProjet/src/users/Consumer.java
+++ DI4MiniProjet/src/users/Consumer.java
@@ -1,5 +1,57 @@
 package users;
 
+import management.Stock;
+
 public class Consumer {
 
+	/*********************************************************************/
+	/***************************** ATTRIBUTES ****************************/
+	/*********************************************************************/
+	private String consName;
+	private Stock stock;
+	
+	
+	/*********************************************************************/
+	/****************************** BUILDERS *****************************/
+	/*********************************************************************/
+	/**
+	 * @param consName
+	 * @param stock
+	 */
+	public Consumer(String consName, Stock stock) {
+		super();
+		this.consName = consName;
+		this.stock = stock;
+	}
+	
+	
+	/*********************************************************************/
+	/***************************** GETS/SETS *****************************/
+	/*********************************************************************/
+	/**
+	 * @return the consName
+	 */
+	public String getConsName() {
+		return consName;
+	}
+	/**
+	 * @param consName the consName to set
+	 */
+	public void setConsName(String consName) {
+		this.consName = consName;
+	}
+	/**
+	 * @return the stock
+	 */
+	public Stock getStock() {
+		return stock;
+	}
+	/**
+	 * @param stock the stock to set
+	 */
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	
+	
 }
--- DI4MiniProjet/src/users/Producer.java
+++ DI4MiniProjet/src/users/Producer.java
@@ -1,5 +1,57 @@
 package users;
 
+import management.Stock;
+
 public class Producer {
 
+	/*********************************************************************/
+	/***************************** ATTRIBUTES ****************************/
+	/*********************************************************************/
+	private String prodName;
+	private Stock stock;
+	
+	
+	/*********************************************************************/
+	/****************************** BUILDERS *****************************/
+	/*********************************************************************/
+	/**
+	 * @param prodName
+	 * @param stock
+	 */
+	public Producer(String prodName, Stock stock) {
+		super();
+		this.prodName = prodName;
+		this.stock = stock;
+	}
+	
+	
+	/*********************************************************************/
+	/***************************** GETS/SETS *****************************/
+	/*********************************************************************/
+	/**
+	 * @return the prodName
+	 */
+	public String getProdName() {
+		return prodName;
+	}
+	/**
+	 * @param prodName the prodName to set
+	 */
+	public void setProdName(String prodName) {
+		this.prodName = prodName;
+	}
+	/**
+	 * @return the stock
+	 */
+	public Stock getStock() {
+		return stock;
+	}
+	/**
+	 * @param stock the stock to set
+	 */
+	public void setStock(Stock stock) {
+		this.stock = stock;
+	}
+	
+	
 }
