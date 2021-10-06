1204818970e2a5b20e5a58e96980d82f42c8e9c1
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-06 14:42:03 +0200

    .

--- DI4MiniProjet/src/management/Stock.java
+++ DI4MiniProjet/src/management/Stock.java
@@ -1,5 +1,77 @@
 package management;
 
-public class Stock {
+import java.util.ArrayList;
 
+public class Stock {
+	
+	/*********************************************************************/
+	/***************************** ATTRIBUTES ****************************/
+	/*********************************************************************/
+	private Integer element;
+	private Integer top;
+	private final Integer size = null;
+	private ArrayList<Integer> stack;
+	
+	
+	/*********************************************************************/
+	/****************************** BUILDERS *****************************/
+	/*********************************************************************/
+	/**
+	 * @param element
+	 * @param top
+	 * @param stack
+	 */
+	public Stock(Integer element, Integer top, ArrayList<Integer> stack) {
+		super();
+		this.element = element;
+		this.top = top;
+		this.stack = stack;
+	}
+	
+	/*********************************************************************/
+	/***************************** GETS/SETS *****************************/
+	/*********************************************************************/
+	/**
+	 * @return the element
+	 */
+	public Integer getElement() {
+		return element;
+	}
+	/**
+	 * @param element the element to set
+	 */
+	public void setElement(Integer element) {
+		this.element = element;
+	}
+	/**
+	 * @return the top
+	 */
+	public Integer getTop() {
+		return top;
+	}
+	/**
+	 * @param top the top to set
+	 */
+	public void setTop(Integer top) {
+		this.top = top;
+	}
+	/**
+	 * @return the stack
+	 */
+	public ArrayList<Integer> getStack() {
+		return stack;
+	}
+	/**
+	 * @param stack the stack to set
+	 */
+	public void setStack(ArrayList<Integer> stack) {
+		this.stack = stack;
+	}
+	/**
+	 * @return the size
+	 */
+	public Integer getSize() {
+		return size;
+	}
+		
 }
