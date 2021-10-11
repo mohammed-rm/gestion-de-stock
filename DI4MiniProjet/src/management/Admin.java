1fac36fdb199ebc131ca323099a9e09c60bcbb7c
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-11 14:51:19 +0200

    createStoc

--- DI4MiniProjet/src/management/Admin.java
+++ DI4MiniProjet/src/management/Admin.java
@@ -4,6 +4,14 @@ import notification.Observer;
 
 public class Admin implements Observer{
 
+	private Integer AdminId;
+	private Stock stock;
+	
+	public void createStock()
+	{
+		stock = new Stock();
+	}
+	
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
