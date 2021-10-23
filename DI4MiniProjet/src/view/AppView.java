5b56cb424bb3b314d7e5480c3d3c71c5e1cfb503
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-23 13:49:25 +0200

    Response windows

--- DI4MiniProjet/src/view/AppView.java
+++ DI4MiniProjet/src/view/AppView.java
@@ -1,24 +1,26 @@
 package view;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.time.LocalTime;
 import java.util.Date;
 
 import javax.swing.GroupLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
-public class AppView extends JFrame {
+public class AppView extends JFrame implements ActionListener {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4040915798387556131L;
+	private static final long serialVersionUID = 1L;
 
 	private JFrame frame;
 	private JPanel panel_1, panel_2, panel_3;
@@ -70,43 +72,37 @@ public class AppView extends JFrame {
 	private static final int TIME_W = 400;
 	private static final int TIME_H = 30;
 
+	private static final int WARNING_MESSAGE_X = 240;
+	private static final int WARNING_MESSAGE_Y = 295;
+	private static final int WARNING_MESSAGE_W = 300;
+	private static final int WARNING_MESSAGE_H = 100;
+
+	private static final int WARNING_ICON_X = 350;
+	private static final int WARNING_ICON_Y = 272;
+	private static final int WARNING_ICON_W = 20;
+	private static final int WARNING_ICON_H = 100;
+
 	private static final String FRAME_TITLE = "Stock Application™ V.1.0";
 	private static final String ADD_BUTTON_NAME = "Add To Stock";
 	private static final String REMOVE_BUTTON_NAME = "Remove From Stock";
 	private static final String EMPTY_BUTTON_NAME = "Empty All Stock";
 
+	/**
+	 * Default GUI Builder
+	 */
 	AppView() {
 
-		// Frame, tab and panels creation
-		frame = new JFrame();
-		frame.pack();
-		panel_1 = new JPanel();
-		panel_2 = new JPanel();
-		panel_3 = new JPanel();
-		tab = new JTabbedPane();
-
-		addField = new JTextField();
-		removeField = new JTextField();
+		init();
 
 		// Configuration
-		frame.setTitle(FRAME_TITLE);
-		frame.setSize(FRAME_SIZE_X, FRAME_SIZE_Y);
-		frame.setResizable(false);
-		frame.setLayout(null);
-		frame.setLocationRelativeTo(null);
+		mainFrameSettings();
 		tab.setBounds(TAB_LOCATION_X, TAB_LOCATION_Y, TAB_SIZE_X, TAB_SIZE_Y);
 
-		// Buttons
-		addButton = new JButton(ADD_BUTTON_NAME);
-		addButton.setBounds(ADD_BUTTON_LOCATION_X, ADD_BUTTON_LOCATION_Y, ADD_BUTTON_SIZE_X, ADD_BUTTON_SIZE_Y);
-
-		removeButton = new JButton(REMOVE_BUTTON_NAME);
-		removeButton.setBounds(REMOVE_BUTTON_LOCATION_X, REMOVE_BUTTON_LOCATION_Y, REMOVE_BUTTON_SIZE_X,
-				REMOVE_BUTTON_SIZE_Y);
-
-		emptyButton = new JButton(EMPTY_BUTTON_NAME);
-		emptyButton.setBounds(EMPTY_BUTTON_LOCATION_X, EMPTY_BUTTON_LOCATION_Y, EMPTY_BUTTON_SIZE_X,
-				EMPTY_BUTTON_SIZE_Y);
+		// Buttons 
+		setButtons(); 
+		addButton.addActionListener(this);
+		removeButton.addActionListener(this);
+		emptyButton.addActionListener(this);
 
 		addField.setBounds(ADD_FIELD_X, ADD_FIELD_Y, ADD_FIELD_W, ADD_FIELD_H);
 		removeField.setBounds(REMOVE_FIELD_X, REMOVE_FIELD_Y, REMOVE_FIELD_W, REMOVE_FIELD_H);
@@ -118,7 +114,7 @@ public class AppView extends JFrame {
 
 		warningText = new JLabel();
 		warningText.setText("All elements will be removed from stock !");
-		warningText.setBounds(240, 295, 300, 100);
+		warningText.setBounds(WARNING_MESSAGE_X, WARNING_MESSAGE_Y, WARNING_MESSAGE_W, WARNING_MESSAGE_H);
 
 		panel_1.setLayout(null); // To change buttons position
 
@@ -132,32 +128,172 @@ public class AppView extends JFrame {
 
 		// Icon path
 		String imgUrl = "images/warning.png";
-		// ImageIcon image = new ImageIcon(imgUrl);
-		// JLabel imageLabel = new JLabel();
-		// imageLabel.setIcon(image);
 
 		// Layout to insert icon
 		GroupLayout layout = new GroupLayout(panel_1);
 		layout.setAutoCreateGaps(true);
 		layout.setAutoCreateContainerGaps(true);
 		JLabel img = new JLabel(new ImageIcon(getClass().getClassLoader().getResource(imgUrl)));
-		img.setBounds(350, 272, 20, 100);
+		img.setBounds(WARNING_ICON_X, WARNING_ICON_Y, WARNING_ICON_W, WARNING_ICON_H);
 
 		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
 		vGroup.addGroup(layout.createParallelGroup().addComponent(img));
 		layout.setVerticalGroup(vGroup);
 
-		// Associate panels to tab
+		// Associate panels to tab 
 		tab.add("Edit Stock", panel_1);
 		tab.add("Show Stock", panel_2);
 		tab.add("About", panel_3);
 
-		// Add the tab to the main frame
+		// Add the tab to the main frame 
 		frame.add(tab);
 
-		// frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+	}
+
+	/**
+	 * Frame, tab and panels creation
+	 */
+	public void init() {
+		frame = new JFrame();
+		frame.pack();
+
+		panel_1 = new JPanel();
+		panel_2 = new JPanel();
+		panel_3 = new JPanel();
+		tab = new JTabbedPane();
+
+		addField = new JTextField();
+		removeField = new JTextField();
+	}
+
+	/**
+	 * 
+	 */
+	public void mainFrameSettings() {
+		frame.setTitle(FRAME_TITLE);
+		frame.setSize(FRAME_SIZE_X, FRAME_SIZE_Y);
+		frame.setResizable(false);
+		frame.setLayout(null);
+		frame.setLocationRelativeTo(null);
+	}
+
+	/**
+	 * 
+	 */
+	public void setButtons() {
+		addButton = new JButton(ADD_BUTTON_NAME);
+		addButton.setBounds(ADD_BUTTON_LOCATION_X, ADD_BUTTON_LOCATION_Y, ADD_BUTTON_SIZE_X, ADD_BUTTON_SIZE_Y);
+
+		removeButton = new JButton(REMOVE_BUTTON_NAME);
+		removeButton.setBounds(REMOVE_BUTTON_LOCATION_X, REMOVE_BUTTON_LOCATION_Y, REMOVE_BUTTON_SIZE_X,
+				REMOVE_BUTTON_SIZE_Y);
+
+		emptyButton = new JButton(EMPTY_BUTTON_NAME);
+		emptyButton.setBounds(EMPTY_BUTTON_LOCATION_X, EMPTY_BUTTON_LOCATION_Y, EMPTY_BUTTON_SIZE_X,
+				EMPTY_BUTTON_SIZE_Y);
+	}
+
+	public void dialogFrame(String message, String iconPath) {
+		JFrame addFrame = new JFrame(FRAME_TITLE);
+		addFrame.setSize(400, 180);
+		addFrame.setResizable(false);
+		addFrame.setLocationRelativeTo(null);
+		addFrame.setVisible(true);
+
+		JLabel l = new JLabel(message, SwingConstants.CENTER);
+		addFrame.setLayout(null);
+		l.setBounds(100, -40, 200, 200);
+
+		// Icon path
+		String imgUrl = iconPath;
+
+		// Layout to insert icon
+		GroupLayout layout = new GroupLayout(addFrame);
+		layout.setAutoCreateGaps(true);
+		layout.setAutoCreateContainerGaps(true);
+		JLabel img = new JLabel(new ImageIcon(getClass().getClassLoader().getResource(imgUrl)));
+		img.setBounds(20, 10, 100, 100);
+
+		GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+		vGroup.addGroup(layout.createParallelGroup().addComponent(img));
+		layout.setVerticalGroup(vGroup);
+		addFrame.add(l);
+	}
+
+	public void dialogFrame() {
+		dialogFrame("Element added succefully", "images/succes.png");
+	}
+
+	public void dialogFrameRemove() {
+		dialogFrame("Element removed succefully", "images/succes.png");
+	}
+
+	public void dialogFrameEmpty() {
+		dialogFrame("Stock is now empty", "images/succes.png");
+
+	}
+
+	public void dialogFrameError() {
+		dialogFrame("An error has occured!", "images/failure.png");
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		Object source = event.getSource();
+		String input = new String();
+		Integer result;
+
+		if (source == addButton) {
+			try {
+				input = addField.getText();
+
+				// afficher le jdialog lorsque le bouton est cliqué
+				JDialog dialog = new JDialog(frame, "Welcome to WayToLearnX", true);
+				dialog.setLocationRelativeTo(frame);
+				dialog.setVisible(true);
+				System.out.print(input);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (source == removeButton) {
+			try {
+				input = addField.getText();
+
+				// afficher le jdialog lorsque le bouton est cliqué
+				JDialog dialog = new JDialog(frame, "Welcome to WayToLearnX", true);
+				dialog.setLocationRelativeTo(frame);
+				dialog.setVisible(true);
+				System.out.print(input);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		else if (source == emptyButton) {
+			try {
+				input = addField.getText();
+
+				// afficher le jdialog lorsque le bouton est cliqué
+				JDialog dialog = new JDialog(frame, "Welcome to WayToLearnX", true);
+				dialog.setLocationRelativeTo(frame);
+				dialog.setVisible(true);
+				System.out.print(input);
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		new AppView();
+
 	}
 
 	/**
@@ -340,10 +476,5 @@ public class AppView extends JFrame {
 	 */
 	public void setWarningText(JLabel warningText) {
 		this.warningText = warningText;
-	}
-
-	public static void main(String[] args) {
-		new AppView();
-
 	}
 }
