5cfaf18851e6eadd63b4220f41e072c88d22d525
Mohammed Rmich <mohammed.rmich@etu.univ-tours.fr>
2021-10-24 10:46:06 +0200

    .

--- DI4MiniProjet/src/management/Admin.java
+++ DI4MiniProjet/src/management/Admin.java
@@ -1,16 +1,42 @@
 package management;
 
 import notification.Observer;
+import users.*;
 
 public class Admin implements Observer{
 
 	private Integer AdminId;
 	private Stock stock;
 	
+	/**
+	 * Allows the administrator to create the stock
+	 */
 	public void createStock()
 	{
 		stock = new Stock();
 	}
+	
+	/**
+	 * Allows the administrator to create a new consumer
+	 * @param name
+	 * @param id
+	 */
+	public void createConsumer(String name, int id)
+	{
+		Consumer consumer = new Consumer(name, id);
+	}
+	
+	/**
+	 * Allows the administrator to create a new producer
+	 * @param name
+	 * @param id
+	 */
+	public void createProducer(String name, int id) {
+		Producer producer = new Producer(name, id);
+	}
+	/**
+	 *
+	 */
 	@Override
 	public void update() {
 		// TODO Auto-generated method stub
